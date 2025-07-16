013eecf1572f2910b1d47c15ac4b8472b27fbfaa
df1gg <samsung26032002@gmail.com>
2025-07-16 11:32:36 +0300

    refactor: moved all texts to text.py

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -1,5 +1,6 @@
 from aiogram import Router, types
 from aiogram.filters import CommandStart
+from bot import text
 from bot.keyboards.main_menu import main_menu_kb
 import config
 
@@ -10,5 +11,5 @@ router = Router()
 @router.message(CommandStart())
 async def start_command_handler(message: types.Message):
     if message.from_user.id != int(config.OWNER_ID):
-        return await message.answer("🚫 Access denied.")
-    await message.answer("✅ Bot is running!", reply_markup=main_menu_kb())
+        return await message.answer(text.ACCESS_DENIED)
+    await message.answer(text.START_TEXT, reply_markup=main_menu_kb())
---
+++ bot/text.py
@@ -0,0 +1,6 @@
+START_TEXT = """🛡️ <b>Welcome to Server Manager Bot!</b>
+
+You can use this bot to monitor and control your Linux server.
+"""
+
+ACCESS_DENIED = "🚫 <b>Access denied.</b> You are not allowed to use this bot."
