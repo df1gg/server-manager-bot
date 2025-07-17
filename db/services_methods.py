1ea1d3b518dce10ac6ce5ec383a4fdc4f8d26f82
df1gg <samsung26032002@gmail.com>
2025-07-17 16:04:13 +0300

    feat: add database with sqlalchemy

---
+++ db/services_methods.py
@@ -0,0 +1,21 @@
+from db.models import Service
+from db.database import async_session_maker
+
+
+async def add_service(name: str, display_name: str = None):
+    async with async_session_maker() as session:
+        service = Service(name=name, display_name=display_name or name)
+        session.add(service)
+        await session.commit()
+
+
+async def get_all_services():
+    async with async_session_maker() as session:
+        result = await session.execute(Service.__table__.select())
+        return result.fetchall()
+
+
+async def delete_service(name: str):
+    async with async_session_maker() as service:
+        await session.execute(Service.__table__.delete().where(Service.name == name))
+        await session.commit()
--- main.py
+++ main.py
@@ -4,10 +4,13 @@ from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from bot import setup_middleware, setup_routers
+from db.database import init_db
 from utils.logger import logger
 
 
 async def main():
+    await init_db()
+
     bot = Bot(
         token=config.BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML)
     )
