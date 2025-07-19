af062398b68861c2b936d5867b37c6b2af5fee5d
df1gg <samsung26032002@gmail.com>
2025-07-19 23:26:27 +0300

    feat: add is_running column to service db

--- db/models.py
+++ db/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Boolean, Column, Integer, String
 from db.base import Base
 
 
@@ -8,6 +8,7 @@ class Service(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String, unique=True, nullable=False)
     display_name = Column(String, nullable=True)
+    is_running = Column(Boolean, default=False)
 
     def __repr__(self):
         return f"<Service(name='{self.name}', display_name='{self.display_name}')>"
